9a43607002b06fd820a3869a332db56e3d919c6b
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-09-24 02:00:52 +0900

    aa

--- src/pages/Training/index.jsx
+++ src/pages/Training/index.jsx
@@ -3,8 +3,15 @@ import TrainderPNG from "../../assets/trainer.png";
 const TrainingHome = () => {
   return (
     <div className="w-full min-h-[100vh]">
-      <div className="">
-        <img src={TrainderPNG} className="w-full" alt="training banner" />
+      <div className="bg-[#80B1FF] min-w-[1440px] min-h-[320px] flex justify-around flex-wrap">
+        <div className="flex flex-col">
+          <span className="text-4xl text-black font-bold text-left">Learn</span>
+          <span className="text-left">
+            Find what fascinates you as you explore crypto
+          </span>
+          <button className="">Join with us</button>
+        </div>
+        <img src={TrainderPNG} className="h-full" alt="training banner" />
       </div>
     </div>
   );
