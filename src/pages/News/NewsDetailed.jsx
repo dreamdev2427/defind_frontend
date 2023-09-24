e4ff8fa4c8a2bd43e0a13bf8350b16694bd2df2a
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-09-25 07:04:46 +0900

    dd

--- src/pages/News/NewsDetailed.jsx
+++ src/pages/News/NewsDetailed.jsx
@@ -4,20 +4,20 @@ const NewsDetails = () => {
   return (
     <div className="w-full min-h-[100vh] flex flex-col items-center">
       <div className="mt-5 container">
-        <div className="flex flex-col items-center">
+        <div className="w-full flex flex-col items-center">
           <img
             src={Secnewbanner}
-            className="w-[850px] h-[380px] rounded-xl "
+            className="w-9/12 h-[380px] rounded-xl "
             alt="news banner"
           />
-          <div className="flex flex-col items-start">
+          <div className="w-9/12 mt-5 flex flex-col items-start">
             <div className="text-[#5BDFE4] text-sm text-left text-medium">
               Crypto News
             </div>
-            <div className="text-2xl font-semibold text-left text-white">
+            <div className="mt-3 text-2xl font-semibold text-left text-white">
               Grayscale Wins Lawsuit Against SEC
             </div>
-            <div className="w-full flex justify-between">
+            <div className="mt-3 w-full flex justify-between">
               <div className="flex justify-start items-center">
                 <div className="text-white text-lg text-left">
                   By Decentralized dog
@@ -27,7 +27,7 @@ const NewsDetails = () => {
                 </div>
               </div>
               <div className="flex justify-end">
-                <div className="">Share:</div>
+                <div className="text-white text-sm">Share:</div>
                 <div className="ml-4 flex gap-3"></div>
               </div>
             </div>
