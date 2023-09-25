b16226a2c5f1616e3fa690c67bffa39363b10b3b
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-09-26 02:27:34 +0900

    update

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -10,24 +10,24 @@ const Favorite = () => {
         </div>
         <div className="flex justify-between  text-white">
           <div className="flex justify-start">
-            <div className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-gradient-to-r from-[#5BDFE4] to-[#024CAD]">
+            <div className="p-3 h-[34px]  flex items-center justify-center   rounded-md bg-gradient-to-r from-[#5BDFE4] to-[#024CAD]">
               All
             </div>
-            <div className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
+            <div className="p-3 h-[34px]  flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
               Memecoins
             </div>
-            <div className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
+            <div className="p-3 h-[34px]  flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
               DeFi
             </div>
-            <div className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
+            <div className=" p-3 h-[34px]  flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
               Airdrop
             </div>
-            <div className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
+            <div className="p-3 h-[34px]  flex items-center justify-center   rounded-md bg-[#1D1D25] ml-2">
               Play2Earn
             </div>
           </div>
           <div className="flex justify-end">
-            <span className="h-[40px] p-3 flex items-center justify-center   rounded-md bg-[#1D1D25]">
+            <span className=" h-[34px]  p-3 flex items-center justify-center   rounded-md bg-[#1D1D25]">
               Delete
             </span>
           </div>
@@ -36,15 +36,15 @@ const Favorite = () => {
         <div className="mt-6 flex flex-wrap justify-between ">
           {exampleItems.map((item, index) => (
             <div key={index} className="pb-9 ">
-              <div className="w-[204px] h-[142px] transform hover:scale-110 transition-transform duration-150 ease-out relative rounded-xl">
+              <div className="w-[200px] h-[140px] transform hover:scale-105 transition-transform duration-150 ease-out relative">
                 <img
                   draggable="false"
-                  className="object-cover w-full h-full rounded-xl"
+                  className="object-cover w-full h-full rounded-xl "
                   src={item.image}
                   alt={item.text}
                 />
                 <div className="bg-gradient-to-t from-[#000000af] w-full h-1/2 absolute bottom-0 z-5 rounded-b-[10px] "></div>
-                <h2 className="text-md font-bold  ml-2 absolute bottom-2 text-white z-10">
+                <h2 className="font-medium  ml-4 absolute bottom-1 text-white z-10">
                   {item.text}
                 </h2>
               </div>
@@ -52,7 +52,33 @@ const Favorite = () => {
                 {item.property.map((x, index) => (
                   <div
                     key={index}
-                    className=" text-sm text-[#aaabb6] bg-[#aaabb63f] p-2 rounded-[6px] ml-1"
+                    className=" text-[12px] font-medium text-[#aaabb6] bg-[#aaabb63f] px-2 py-1 rounded-[8px] ml-1"
+                  >
+                    {x}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+          {exampleItems.map((item, index) => (
+            <div key={index} className="pb-9 ">
+              <div className="w-[200px] h-[140px] transform hover:scale-105 transition-transform duration-150 ease-out relative">
+                <img
+                  draggable="false"
+                  className="object-cover w-full h-full rounded-xl "
+                  src={item.image}
+                  alt={item.text}
+                />
+                <div className="bg-gradient-to-t from-[#000000af] w-full h-1/2 absolute bottom-0 z-5 rounded-b-[10px] "></div>
+                <h2 className="font-medium  ml-4 absolute bottom-1 text-white z-10">
+                  {item.text}
+                </h2>
+              </div>
+              <div className="flex gap-1 mt-3">
+                {item.property.map((x, index) => (
+                  <div
+                    key={index}
+                    className=" text-[12px] font-medium text-[#aaabb6] bg-[#aaabb63f] px-2 py-1 rounded-[8px] ml-1"
                   >
                     {x}
                   </div>
